04d16e33942040194c82c06562dd839b9d5d766d
khansmoody <peace13589@gmail.com>
2026-02-05 16:21:12 -0500

    Feat: Add job searching feature

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Job(models.Model):
+
+    # Basic work info
+    title = models.CharField(max_length=200)
+    company_name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    location = models.CharField(max_length=100)
+    skills = models.CharField(max_length=250)
+    
+    # Option to select
+    WORK_TYPE_CHOICES = [
+        ('remote', 'Remote'),
+        ('onsite', 'On-site'),
+        ('hybrid', 'Hybrid'),
+    ]
+    work_type = models.CharField(max_length=10, choices=WORK_TYPE_CHOICES, default='onsite')
+    
+    # Salary and Visa
+    salary_min = models.IntegerField(default=0)
+    salary_max = models.IntegerField(default=0)
+    visa_sponsorship = models.BooleanField(default=False)  # 비자 지원 여부
+    
+    # Management information
+    recruiter = models.ForeignKey(settings.AUTH_USER_MODEL, on_express_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} at {self.company_name}"
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Job
 
-# Create your views here.
+def job_list(request):
+    jobs = Job.objects.all()
+    
+    # Bring Filetering value
+    title_query = request.GET.get('title')
+    location_query = request.GET.get('location')
+    min_salary = request.GET.get('min_salary')
+    visa_support = request.GET.get('visa') # 'on' or None
+
+    # Add filter only if there are conditions
+    if title_query:
+        jobs = jobs.filter(Q(title__icontains=title_query) | Q(skills__icontains=title_query))
+    if location_query:
+        jobs = jobs.filter(location__icontains=location_query)
+    if min_salary:
+        jobs = jobs.filter(salary_min__gte=min_salary)
+    if visa_support == 'on':
+        jobs = jobs.filter(visa_sponsorship=True)
+
+    return render(request, 'jobs/job_list.html', {'jobs': jobs})
