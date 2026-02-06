0595ba631511784bae769972c154ad03d6ccc675
Mai Tran <maitran@Mais-MacBook-Air.local>
2026-02-06 13:44:11 -0500

    ADD: updated urls.py with jobseeker profile routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path('signup', views.signup, name='accounts.signup'),
     path('login/', views.login, name='accounts.login'),
     path('logout/', views.logout, name='accounts.logout'),
+    path('profile/', views.profile_view, name='accounts.profile_me'),
+    path('profile/edit/', views.profile_edit, name='accounts.profile_edit'),
+    path('profile/<str:username>/', views.profile_view, name='accounts.profile_user'),
 ]
